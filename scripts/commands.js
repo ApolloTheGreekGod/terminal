f5f3a81045ed0c0e4a4d0faf2e081b1a31f994d3
ApolloTheGreekGod <63588755+ApolloTheGreekGod@users.noreply.github.com>
2021-10-13 13:51:44 +0800

    v2

--- scripts/commands.js
+++ scripts/commands.js
@@ -1,8 +1,41 @@
 $('body').terminal({
-    hello: function(what) {
-        this.echo('Hello, ' + what +
-                  '. Wellcome to this terminal.');
+    hello: function() {
+        this.echo('Hello, there. Wellcome to this terminal.');
+    },
+    cat: function(width, height) {
+        const img = $('<img src="https://placekitten.com/' +
+                      width + '/' + height + '">');
+        this.echo(img);
+    },
+    help: function() {
+        this.echo(` Commands : 
+        help - help menu 
+        hello - Greet the terminal
+        about - Info about this terminal
+        cat [width] [height] - generates random cat pictures
+        `)
+    },
+    info: function() {
+        this.echo(`
+        Terminal Info : 
+        Name : Apollo
+        Creator : Rayden Ooi
+        Made with JQuery, JavaScript, HTML, CSS`)
     }
-}, {
-    greetings: 'My First Web Terminal'
+},
+ {
+    greetings:
+ `      $$$$$$\        $$$$$$$\          $$$$$$\         $$\              $$\               $$$$$$\  
+     $$  __$$\       $$  __$$\        $$  __$$\        $$ |            $$ |            $$  __$$\ 
+    $$ /  $$ |      $$ |  $$ |      $$ /  $$ |      $$ |            $$ |            $$ /  $$ |
+    $$$$$$$$ |      $$$$$$$  |      $$ |  $$ |      $$ |            $$ |            $$ |  $$ |
+    $$  __$$ |      $$  ____/       $$ |  $$ |      $$ |            $$ |            $$ |  $$ |
+    $$ |  $$ |      $$ |            $$ |  $$ |      $$ |            $$ |            $$ |  $$ |
+    $$ |  $$ |      $$ |             $$$$$$  |      $$$$$$$$\       $$$$$$$$\          $$$$$$  |
+    \__|  \__|      \__|             \______/       \________|      \________|       \______/ 
+
+
+    Welcome to my Terminal-like Website! Type 'help' to see a list of commands.
+    `
+
 });

d07a53a2899a86d03841ee15a2661945dacc31ff
ApolloTheGreekGod <63588755+ApolloTheGreekGod@users.noreply.github.com>
2021-10-27 22:16:49 +0800

    added more jokes

--- scripts/jokes.js
+++ scripts/jokes.js
@@ -1,7 +1,48 @@
 const jokes = [`What is the best thing about Switzerland?
-I do not know, but the flag is a big plus.`, `I invented a new word!
-Plagiarism!`, `Did you hear about the mathematician who is afraid of negative numbers?
-He will stop at nothing to avoid them.`, `Why do we tell actors to 'break a leg?'
-Because every play has a cast.`, `Helvetica and Times New Roman walk into a bar.
-'Get out of here!' shouts the bartender. 'We don’t serve your type.',`, `Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, 'What is the word on the street?'
-Once my dog ate all the Scrabble tiles. For days he kept leaving little messages around the house.`]
+I do not know, but the flag is a big plus.`,
+ `I invented a new word!
+Plagiarism!`,
+ `Did you hear about the mathematician who is afraid of negative numbers?
+He will stop at nothing to avoid them.`,
+ `Why do we tell actors to 'break a leg?'
+Because every play has a cast.`, 
+`Helvetica and Times New Roman walk into a bar.
+'Get out of here!' shouts the bartender. 'We don't serve your type.',`, 
+`Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, 'What is the word on the street?'
+Once my dog ate all the Scrabble tiles. For days he kept leaving little messages around the house.`,
+`Knock! Knock!
+Who is there?
+Control Freak.
+Con…
+OK, now you say, 'Control Freak who?' `,
+`Hear about the new restaurant called Karma?
+There is no menu: You get what you deserve.`,
+`A woman in labor suddenly shouted, 'Shouldn't! Wouldn't! Couldn't! Didn't! Can't!'
+'Don't worry,' said the doc. 'Those are just contractions.'`,
+`A bear walks into a bar and says, 'Give me a whiskey and … cola.'
+'Why the big pause?' asks the bartender. The bear shrugged. 'I am not sure; I was born with them.'`,
+`Did you hear about the actor who fell through the floorboards?
+He was just going through a stage.`,
+`Did you hear about the claustrophobic astronaut?
+He just needed a little space.`,
+`Why don't scientists trust atoms?
+Because they make up everything.`,
+`Why did the chicken go to the séance?
+To get to the other side. `,
+`Where are average things manufactured?
+The satisfactory.`,
+`How do you drown a hipster?
+Throw him in the mainstream.`,
+`What sits at the bottom of the sea and twitches?
+A nervous wreck.`,
+`What does a nosy pepper do?
+Gets jalapeño business!`,
+`How does Moses make tea?
+He brews.`,
+`Why can't you explain puns to kleptomaniacs?
+They always take things literally.`,
+`How do you keep a bagel from getting away?
+Put lox on it.`,
+`A man tells his doctor, 'Doc, help me. I'm addicted to Twitter!'
+The doctor replies, 'Sorry, I don't follow you …'`
+]
